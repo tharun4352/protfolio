f31a307364c48f972951a065a03461571296b978
tharun4352 <tharuninjothi@gmail.com>
2026-06-12 13:39:40 +0530

    Add typing animation and particle effects to script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+new Typed("#typing",{
+strings:[
+"Aspiring AI Engineer",
+"Machine Learning Developer",
+"Data Science Enthusiast",
+"Future ML Researcher"
+],
+typeSpeed:70,
+backSpeed:50,
+loop:true
+});
+
+particlesJS("particles-js",{
+particles:{
+number:{value:90},
+size:{value:3},
+move:{speed:2},
+line_linked:{
+enable:true
+}
+}
+});
